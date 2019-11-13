74d707f39cdcb0d1643d79781fb4b7b266297d44
Sander Furre <sander.furre@gmail.com>
2019-11-14 00:34:43 +0100

    Simulink diagrams Dag5, cleanup and file fix of simulink diagrams 81->8

--- HeliLabb Dag2/init_heli.m
+++ HeliLabb Dag2/init_heli.m
@@ -32,27 +32,57 @@ k_1 = k_f/(2*m_p*l_p); %
 k_2 = (l_h*k_f)/(m_c*l_c^2+2*m_p*l_h^2); 
 k_3 = (v_s0*l_h*k_f)/(m_c*l_c^2+2*m_p*(l_h^2+l_p^2));
 
-problem_to_run=input('What problem to run?');
+% problem_to_run=input('What problem to run?');
 
 k_pp=0;
 k_pd=0;
 
-%pole_plotter(2,k_1);
-%pole_finder(6,0,k_1)
 
-if problem_to_run==2
-    fprintf('Running problem 2\n');
-    k_pp=input('Value for k_pp: ');
-    k_pd=input('Value for k_pd: ');
-elseif problem_to_run==3
-    fprintf('Running problem 3\n');
-    omega_0 = input('Value for omega_0: ');
-    zeta = input('Value for zeta: ');
-    k_pp = omega_0^2/k_1;
-    k_pd = (2*zeta*omega_0)/k_1;
-else
-    fprintf('No valid porblem\n');
-end
+%pole_plotter(2,k_1);
+% problem_number=2;
+% fprintf('Enjoy plotting some poles <3');
+% f1 = figure('Name','Pole placement');
+% grid on;
+% hold on;
+% number_of_pole_sets = input('How many pole sets to plot: ');
+% plotHandles = zeros(1,number_of_pole_sets);
+% plotLabels = cell(1,number_of_pole_sets);
+% colors = ['red','blue'];
+% for i=1:number_of_pole_sets
+%     k_pp=0;
+%     k_pd=0;
+%     if problem_number==2
+%         k_pp=input('Value for k_pp: ');
+%         k_pd=input('Value for k_pd: ');
+%         [s1,s2] = pole_finder(k_pp,k_pd,k_1);
+%         s1_real = real(s1);
+%         s1_imag = imag(s1);
+%         s2_real = real(s2);
+%         s2_imag = imag(s2);
+%         kpp_str = int2str(k_pp);
+%         kpd_str = int2str(k_pd);
+%         legend_string=strcat('Kpp=',kpp_str,' ','Kpd=',kpd_str);
+%         plotHandles(i) = scatter([s1_real,s2_real],[s1_imag,s2_imag],200,colors(:,i),'X');
+%         plotLabels{i} = legend_string;
+%     end
+% end
+% legend(plotHandles,plotLabels);
+% line([0,0], ylim, 'Color', 'k', 'LineWidth', 1); %Y_axis
+% line(xlim, [0,0], 'Color', 'k', 'LineWidth', 1); %X-axis 
+
+% if problem_to_run==2
+%     fprintf('Running problem 2\n');
+%     k_pp=input('Value for k_pp: ');
+%     k_pd=input('Value for k_pd: ');
+% elseif problem_to_run==3
+%     fprintf('Running problem 3\n');
+%     omega_0 = input('Value for omega_0: ');
+%     zeta = input('Value for zeta: ');
+%     k_pp = omega_0^2/k_1;
+%     k_pd = (2*zeta*omega_0)/k_1;
+% else
+%     fprintf('No valid porblem\n');
+% end
 
 
 
--- HeliLabb Dag2/pole_plotter.m
+++ HeliLabb Dag2/pole_plotter.m
@@ -1,6 +1,9 @@
 function pole_plotter(problem_number,k_1)
 fprintf('Enjoy plotting some poles <3');
-%f1 = figure('Poles placement');
+f1 = figure('Name','Pole placement');
+grid on;
+hold on;
+legend_array=[];
 number_of_pole_sets = input('How many pole sets to plot: ');
 for i=1:number_of_pole_sets
     k_pp=0;
@@ -8,11 +11,21 @@ for i=1:number_of_pole_sets
     if problem_number==2
         k_pp=input('Value for k_pp: ');
         k_pd=input('Value for k_pd: ');
-        s1 = pole_finder(k_pp,k_pd,k_1);
-        fprintf(s1);
+        [s1,s2] = pole_finder(k_pp,k_pd,k_1);
+        s1_real = real(s1);
+        s1_imag = imag(s1);
+        s2_real = real(s2);
+        s2_imag = imag(s2);
+        scatter([s1_real,s2_real],[s1_imag,s2_imag],200,'X');
+        kpp_str = int2str(k_pp);
+        kpd_str = int2str(k_pd);
+        legend_string=strcat('Kpp=',kpp_str,' ','Kpd=',kpd_str);
+        legend_array = [legend_array legend_string];
     end
 end
-
+legend(legend_array);
+line([0,0], ylim, 'Color', 'k', 'LineWidth', 1); %Y_axis
+line(xlim, [0,0], 'Color', 'k', 'LineWidth', 1); %X-axis 
 
 
 end
